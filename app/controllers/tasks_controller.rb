860ad7d54792c692ffc8d30e510ff6accd8c22aa
yodial94 <alghiffaari17@gmail.com>
2021-03-20 14:46:02 +0700

    adding fields status task and tasklist form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_project
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
@@ -25,7 +26,7 @@ class TasksController < ApplicationController
     @task = @project.tasks.build(task_params)
 
     if @task.save
-      redirect_to([@task.project, @task], notice: 'Task was successfully created.')
+      redirect_to(@task.project, notice: 'Task was successfully created.')
     else
       render action: 'new'
     end
@@ -33,8 +34,8 @@ class TasksController < ApplicationController
 
   # PUT projects/1/tasks/1
   def update
-    if @task.update_attributes(task_params)
-      redirect_to([@task.project, @task], notice: 'Task was successfully updated.')
+    if @task.update(task_params)
+      redirect_to(@task.project, notice: 'Task updated.')
     else
       render action: 'edit'
     end
@@ -44,13 +45,13 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
 
-    redirect_to project_tasks_url(@project)
+    redirect_to @project
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
-      @project = Project.find(params[:project_id])
+      @project = current_user.projects.find(params[:project_id])
     end
 
     def set_task
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ApplicationRecord
   belongs_to :project
+
+  validates :status, inclusion: { in: ['not-started', 'in-progress', 'complete'] }
+
+  STATUS_OPTION = [
+  	['Not started', 'not-started'],
+  	['In progress', 'in-progress'],
+  	['Complete', 	'complete']
+  ]
 end
