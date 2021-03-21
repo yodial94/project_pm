454a432facb230d726cab684eadeb69395f6d2e9
yodial94 <alghiffaari17@gmail.com>
2021-03-21 23:38:05 +0700

    Adding metode for showing Project and task status complete

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,4 +8,29 @@ class Task < ApplicationRecord
   	['In progress', 'in-progress'],
   	['Complete', 	'complete']
   ]
+
+  def badge_color
+    case status
+    when 'not-started'
+      'danger'
+    when 'in-progress'
+      'info'
+    when 'complete'
+      'success'
+    end
+  end
+
+
+  def complete?
+    status == 'complete'
+  end
+
+   def in_progress?
+    status == 'in-progress'
+  end
+
+   def not_started?
+    status == 'not-started'
+  end
+
 end
