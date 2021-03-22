e7f52b0d060920c3a014b8f87deb3065531e1de3
yodial94 <alghiffaari17@gmail.com>
2021-03-22 12:33:30 +0700

    adding column and show priority on view tasks list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_181156) do
+ActiveRecord::Schema.define(version: 2021_03_21_213342) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_03_19_181156) do
     t.integer "project_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "priority"
     t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
